69667cee79fc5dba53c2e8961b8f0e994c909d46
AIMLTeam <kolikaran1992@gmail.com>
2020-02-10 00:20:40 +0530

    Chap1: Q1-13

--- utils.py
+++ utils.py
@@ -3,26 +3,32 @@ import numpy as np
 from collections import Counter
 
 
-def _vline(obj):
+def rollling_avg(arr):
+    cumsum = np.cumsum(arr, axis=0)
+    lengths = np.repeat(np.arange(1,arr.shape[0]+1, 1).reshape(1,-1), arr.shape[1], axis=0).T
+    return cumsum/lengths
+
+
+def vline_(obj):
 	if not obj:
 		return
-	plt.axvline(obj['pos'], color='black', linestyle='--', marker=0, linewidth=5)
+	plt.axvline(obj['pos'], color='black', linestyle='--', marker=0, linewidth=2)
 	plt.annotate(obj['msg'], 
 	             xy=obj['xy'], 
 	             xytext=obj['xytext'],
 	             arrowprops=dict(facecolor='black', shrink=2))
 
-def _hline(obj):
+def hline_(obj):
 	if not obj:
 		return
-	plt.axhline(obj['pos'], color='black', linestyle='--', marker=0, linewidth=3)
+	plt.axhline(obj['pos'], color='black', linestyle='--', marker=0, linewidth=2)
 	plt.annotate(obj['msg'], 
 	             xy=obj['xy'], 
 	             xytext=obj['xytext'],
 	             arrowprops=dict(facecolor='black', shrink=2))
 
 
-def _labels(
+def labels(
 		title=None,
 		xlabel=None,
 		ylabel=None
@@ -40,52 +46,75 @@ def plot_hist(
 			title=None,
 			xlabel=None,
 			ylabel=None,
-			hline=None
+			hline=None,
+			rotate=False
+
 		):
 	num_trials = len(data)
 	obj = plt.hist(data, bins=num_bins,
-        edgecolor='black', linewidth=1.5)
-	plt.xticks(
-		ticks=obj[1].tolist(), 
-		labels=list(map(lambda x: '{:.1f}'.format(x), obj[1].tolist())), 
-		rotation=90)
-	plt.yticks(ticks=range(num_trials//num_y_ticks,num_trials+num_trials//num_y_ticks, num_trials//num_y_ticks), 
-	           labels=range(100//num_y_ticks,100+100//num_y_ticks, 100//num_y_ticks))
-	plt.grid(True, axis='y')
+        edgecolor='black', linewidth=1.5, orientation='horizontal' if rotate else None)
+	if not rotate:
+		plt.xticks(
+			ticks=obj[1].tolist(), 
+			labels=list(map(lambda x: '{:.1f}'.format(x), obj[1].tolist())), 
+			rotation=90)
+		plt.yticks(ticks=range(num_trials//num_y_ticks,num_trials+num_trials//num_y_ticks, num_trials//num_y_ticks), 
+		           labels=range(100//num_y_ticks,100+100//num_y_ticks, 100//num_y_ticks))
+		plt.grid(True, axis='y')
+	else:
+		plt.yticks(
+			ticks=obj[1].tolist(), 
+			labels=list(map(lambda x: '{:.1f}'.format(x), obj[1].tolist())), 
+			rotation=90)
+		plt.xticks(ticks=range(num_trials//num_y_ticks,num_trials+num_trials//num_y_ticks, num_trials//num_y_ticks), 
+		           labels=range(100//num_y_ticks,100+100//num_y_ticks, 100//num_y_ticks))
+		plt.grid(True, axis='x')
 
-	_hline(hline)
-	_labels(title=title, xlabel=xlabel, ylabel=ylabel)
+	hline_(hline)
+	labels(title=title, xlabel=xlabel, ylabel=ylabel)
 
 def plot_bar(
 		data=None,
-		num_y_ticks=None,
+		num_trials=None,
+		num_y_ticks=10,
 		title=None,
 		xlabel=None,
 		ylabel=None,
-		hline=None
+		hline=None,
+		cap=1
 	):
-	num_trials = len(data)
-	_temp = Counter(data)
+	if not num_trials:
+		num_trials = len(data)
+		_temp = Counter(data)
+	else:
+		_temp = data
 
 	obj = plt.bar(_temp.keys(), _temp.values(),
-        edgecolor='black', linewidth=1.5)
+        edgecolor='black', linewidth=1.5, tick_label=list(_temp.keys()))
+
+	num_trials = int(cap*num_trials)
 
 	plt.yticks(ticks=range(num_trials//num_y_ticks,num_trials+num_trials//num_y_ticks, num_trials//num_y_ticks), 
-	           labels=range(100//num_y_ticks,100+100//num_y_ticks, 100//num_y_ticks))
+	           labels=range(int(100*cap)//num_y_ticks,int(100*cap)+int(100*cap)//num_y_ticks, int(100*cap)//num_y_ticks))
 	plt.grid(True, axis='y')
 
-	_hline(hline)
-	_labels(title=title, xlabel=xlabel, ylabel=ylabel)
+	hline_(hline)
+	labels(title=title, xlabel=xlabel, ylabel=ylabel)
 
 def plot_line(
 		data=None,
 		title=None,
 		xlabel=None,
 		ylabel=None,
-		hline=None
+		hline=None,
+		vline=None,
+		thickness=1,
+		label=None
 	):
 	num_trials = len(data)
-	plt.plot(range(num_trials), data, '-')
-	_hline(hline)
-	_labels(title=title, xlabel=xlabel, ylabel=ylabel)
+	line = plt.plot(range(num_trials), data, '-', linewidth=thickness, label=label)
+	hline_(hline)
+	vline_(vline)
+	labels(title=title, xlabel=xlabel, ylabel=ylabel)
 	plt.grid(True)
+	return line
